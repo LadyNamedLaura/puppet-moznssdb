b4cf69d001b18690d636c1b86972a44601c62c67
Simon Peeters <simon@inuits.eu>
2016-06-28 16:02:09 +0200

    add nssdb_key type
    allows managing private keys in the db

---
+++ lib/puppet/type/nssdb_key.rb
@@ -0,0 +1,31 @@
+Puppet::Type.newtype(:nssdb_key) do
+  ensurable
+  feature :fileinput, "read cert from local file"
+
+  newparam(:name, :namevar => true) do
+  end
+
+  newparam(:alias) do
+    isrequired
+  end
+  newparam(:dbpath) do
+    isrequired
+    desc "The path to the database."
+  end
+
+  newproperty(:path, :required_features => %w{fileinput}) do
+  end
+  newproperty(:content) do
+    munge do |value|
+      value.strip.gsub(/[\n\r]+/, "\n")
+    end
+  end
+
+  validate do
+    if @parameters[:content].nil? == @parameters[:path].nil?
+      self.fail("provide one of content or path")
+    end
+
+    provider.validate if provider.respond_to?(:validate)
+  end
+end
